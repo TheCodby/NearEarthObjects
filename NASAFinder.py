b28c526ed72c5704f657c31e8422abc615db94e7
Codby <87335804+TheCodby@users.noreply.github.com>
2021-09-10 14:22:25 +0300

    Add files via upload

---
+++ NASAFinder.py
@@ -0,0 +1,15 @@
+import requests
+import pandas as pd
+minDate = input('''Min Date
+Exp: 1900-01-01
+''')
+maxDate = input('''Min Date
+Exp: 2100-01-01
+''')
+MaxDistance = input('''Max Distance
+Exp: 0.2
+''')
+response = requests.get('https://ssd-api.jpl.nasa.gov/cad.api?des=433&date-min='+ minDate + '&date-max='+ maxDate + '&dist-max=0.2')
+res = response.json()
+df3 = pd.DataFrame(res['data'], columns=res['fields'])
+print(df3)

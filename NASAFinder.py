bb4aead1153fc166c80e2ad807953681a4283f1d
Codby <87335804+TheCodby@users.noreply.github.com>
2021-11-14 14:46:06 +0300

    Update NASAFinder.py

--- NASAFinder.py
+++ NASAFinder.py
@@ -9,7 +9,7 @@ Exp: 2100-01-01
 MaxDistance = input('''Max Distance
 Exp: 0.2
 ''')
-response = requests.get('https://ssd-api.jpl.nasa.gov/cad.api?des=433&date-min='+ minDate + '&date-max='+ maxDate + '&dist-max=0.2')
+response = requests.get('https://ssd-api.jpl.nasa.gov/cad.api?des=433&date-min='+ minDate + '&date-max='+ maxDate + '&dist-max=' + MaxDistance)
 res = response.json()
 df3 = pd.DataFrame(res['data'], columns=res['fields'])
 print(df3)
